f9c160cf8c7a5b572c2d90944c968083d2ea490d
Marjona Said Jamol <57765810+msj1234@users.noreply.github.com>
2022-01-08 14:18:50 -0500

    Update project.py

--- project.py
+++ project.py
@@ -4,23 +4,32 @@
 #testing commit/push
 
 #opemimg amd readimg the file
-f = open('user.txt', 'r')
+file = open('user.txt', 'r')
 #for loop to loop through text file
-for line in f:
+for line in file:
   fields = line.split(",") #spilt using ,
-  name = feilds[0]
-  age = feilds[1]
-  status = feilds[2]
-  hobby = feilds[3]
-  study = feilds[4]
+  name = fields[0]
+  age = fields[1]
+  status = fields[2]
+  hobby = fields[3]
+  study = fields[4]
   
   #for test purposes 
   print("print info \n")
   print(name \n)
   print(age)
   
+  
 # --- Matching Algorithm --- #
 # for the specified mentee
 #   loop through mentors
 #       if interests match, age is in range (mentor 5-10 yrs older??), locations near (etc?)
 #           add mentor to list of matches (mentee gets to choose who they want to talk to)
+
+#if mentor, loop through to find mentee with similar hobbies 
+if(status = 'mentor'):
+  #loop through array etc..
+  continue
+else: # if mentee then conect with mentor with same interests 
+  continue 
+file.close()
