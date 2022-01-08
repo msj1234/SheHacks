5ef3002c8e0bf284069281b95b4514930b673db4
Jessica Wong <jessicaa.wong@mail.utoronto.ca>
2022-01-08 12:03:43 -0500

    pseudocode matching algorithm

--- project.py
+++ project.py
@@ -4,3 +4,10 @@ name = ("Please enter name")
 print(name)
 
 #testing commit/push
+
+
+# --- Matching Algorithm --- #
+# for the specified mentee
+#   loop through mentors
+#       if interests match, age is in range (mentor 5-10 yrs older??), locations near (etc?)
+#           add mentor to list of matches (mentee gets to choose who they want to talk to)
