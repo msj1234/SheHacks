b6f06e26621b0ebe260a150dbc6b72d8857db2fe
Jessica Wong <jessicaa.wong@mail.utoronto.ca>
2022-01-08 12:33:49 -0500

    person class

---
+++ person.py
@@ -0,0 +1,11 @@
+class person():
+    def __init__(self, name, age, status, hobby, field):
+        self.name = name
+        self.age = age
+        self.status = status
+        self.hobby = hobby
+        self.field = field
+
+
+
+
